1e903222ef37749a7bf4e9041cd0919d1707cd8d
Daniel Bui <dnlbui@proton.me>
2022-12-26 19:06:55 -0600

    added post book route testing

--- test/book.js
+++ test/book.js
@@ -8,6 +8,10 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
+beforeEach(() => {
+  Book.removeAll();
+})
+
 describe("Books", () => {
   describe("/GET book", () => {
     it("it should GET all the books", (done) => {
@@ -19,7 +23,51 @@ describe("Books", () => {
           res.body.should.be.an("array");
           res.body.length.should.be.eql(0);
           done();
+        });
+    });
+  });
+
+  describe("/POST book", () => {
+    it("it should POST a book", done => {
+      //arrange
+      let book = {
+        title: "The Hunger Games",
+        author: "Suzanne Collins",
+        year: 2008,
+        pages: 301
+      }
+      //act
+      chai
+        .request(server)
+        .post('/book')
+        .send(book)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property("title");
+          res.body.should.have.property("author");
+          res.body.should.have.property("pages");
+          res.body.should.have.property("year");
+          done();
+        })
+    });
+    it("should not POST a book without pages field", done => {
+      //arrange
+      let book ={
+        title: "The Hunger Games",
+        author: "Suzanne Collins",
+        year: 2008,
+        pages: 0 
+      }
+      //act
+      chai
+        .request(server)
+        .post("/book")
+        .send(book)
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
         })
     })
-  })
-})
+  });
+});
